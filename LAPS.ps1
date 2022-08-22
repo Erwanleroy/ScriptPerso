8a6d94505b587b7d2fa68a06da601fb7631a24d8
NovaZer <41429281+Erwanleroy@users.noreply.github.com>
2022-08-22 16:22:00 +0200

    Récupération du mdp admin local avec LAPS-UI

---
+++ LAPS.ps1
@@ -0,0 +1,20 @@
+﻿#Récupération du mdp admin local avec LAPS-UI 
+function CaCopieDur($text) {
+	Add-Type -AssemblyName System.Windows.Forms
+	$tb=New-Object System.Windows.Forms.TextBox
+	$tb.Multiline = $true
+	$tb.Text = $text
+	$tb.SelectAll()
+	$tb.Copy()
+}
+$nomDuPc = Read-Host "Le nom du PC "
+try {
+    $pwdAdmLocal = Get-ADComputer $nomDuPc -Properties * | select -ExpandProperty ms-Mcs-AdmPwd
+    write-host $pwdAdmLocal -foreground green
+    CaCopieDur($pwdAdmLocal)
+}
+catch {
+    write-host "Nom de PC introuvable" -foreground red
+}
+pause
+cls
