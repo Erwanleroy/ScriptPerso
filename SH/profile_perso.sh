e1a9d0d3047a123d8d3b6b571e508c6dd712c7c7
NovaZer <41429281+Erwanleroy@users.noreply.github.com>
2025-06-25 12:06:29 +0200

    Ajout RC

--- SH/profile_perso.sh
+++ SH/profile_perso.sh
@@ -9,70 +9,65 @@
 #       Date    : 06/02/2025 - V1 Init
 #_______________________________________________________
 #
-#_________________________
-#
-#       Fonctions
-#_________________________
-#
-# Efface l'écran et désactive le timeout
-clear
-unset TMOUT
+clear; unset TMOUT; 
+[ "$(whoami)" = "root" ] && c=1 || c=5; 
 
-# Définit la variable c selon que l'utilisateur est root ou non
-[ "$(whoami)" = "root" ] && c=1 || c=5
+update_prompt() { 
+    local rc=$?; 
+    PS1="\n\[\e[36m\][\[\e[0m\]\[\e[1m\e[32m\] $(hostname) \[\e[0m\]\[\e[36m\]|\[\e[0m\]\[\e[1m\e[3${c}m\] $(whoami) \[\e[0m\]\[\e[36m\]|\[\e[0m\]\[\e[1m\e[34m\] \$(pwd) - (\$(ls | wc -l) items) \[\e[0m\]\[\e[36m\]|\[\e[0m\]\[\e[1m\e[33m\] \$(date +'%H:%M:%S') "
+    if [[ $rc -ne 0 ]]; then
+        PS1+="\[\e[0m\]\[\e[36m\]|\[\e[0m\]\[\e[1m\e[31m\] RC=$rc "
+    fi
+    PS1+="\[\e[0m\]\[\e[36m\]]\n>\[\e[0m\] "
+}; 
 
-# Configure le prompt de la ligne de commande (PS1)
-PS1="\n\[\e[36m\][\[\e[0m\]\[\e[1m\e[32m\] $(hostname) \[\e[0m\]\[\e[36m\]|\[\e[0m\]\[\e[1m\e[3${c}m\] $(whoami) \[\e[0m\]\[\e[36m\]|\[\e[0m\]\[\e[1m\e[34m\] \$(pwd) - (\$(ls | wc -l) items) \[\e[0m\]\[\e[36m\]|\[\e[0m\]\[\e[1m\e[33m\] \$(date +'%H:%M:%S') \[\e[0m\]\[\e[36m\]]\n>\[\e[0m\] "
-
-# Définit des alias
+# Alias pour navigation et affichage
 alias ..="cd .."
 alias ll="ls -lahrt"
 alias tree="tree -FalxhtC --du"
 alias df="df -Ph --total"
 
-# Déclare un tableau pour stocker les répertoires visités
-declare -a _D
+# Gestionnaire de répertoires
+declare -a _D; 
 
-# Fonction pour sauvegarder le répertoire courant
-_sv() {
-    local i
-    for ((i=0; i<=9; i++)); do
-        [ "$1" = "${_D[$i]}" ] && return
-    done
-    for ((i=9; i>0; i--)); do
-        _D[$i]="${_D[((i-1))]}"
-    done
-    _D[0]="$1"
-}
+_sv() { 
+    local i; 
+    for ((i=0;i<=9;i++)); do 
+        [ "$1" = "${_D[$i]}" ] && return; 
+    done; 
+    for ((i=9;i>0;i--)); do 
+        _D[$i]="${_D[((i-1))]}"; 
+    done; 
+    _D[0]="$1"; 
+}; 
 
-# Fonction pour afficher les répertoires sauvegardés
-_sh() {
-    local i=0
-    while [ "${_D[$i]}" ]; do
-        echo -e "\033[0;34m$i:\033[0;33m - ${_D[$i]}\033[0m"
-        ((i++))
-    done
-}
+_sh() { 
+    local i=0; 
+    local c=0; 
+    while [ "${_D[$i]}" ]; do 
+        echo -e "\033[0;34m$i:\033[0;33m - ${_D[$i]}\033[0m"; 
+        ((i++)); 
+    done; 
+}; 
 
-# Fonction pour changer de répertoire à partir de l'historique
-_gd() {
-    local d
-    _sh
-    printf "\nchoix : "
-    read -n 1 d
-    echo
-    cd "${_D[$d]}"
-}
+_gd() { 
+    local d; 
+    _sh; 
+    printf "\nchoix : "; 
+    read -n 1 d; 
+    echo; 
+    cd "${_D[$d]}"; 
+}; 
 
-# Configure la commande à exécuter avant chaque invite
-PROMPT_COMMAND='_sv "$OLDPWD"'
+# Mise à jour du prompt et gestionnaire de répertoires
+PROMPT_COMMAND='update_prompt; _sv "$OLDPWD"'; 
 
-# Alias pour changer de répertoire avec historique
-alias cdh=_gd
+# Alias pour navigation
+alias cdh=_gd; 
 
-# Configure le format de l'historique
+# Variables d'historique
 export HISTTIMEFORMAT="-- %Y/%m/%d %H:%M:%S -- "
-export HISTCONTROL=ignoredups:ignorespace
+export HISTCONTROL=ignoredups:ignorespace; 
 
-# Alias pour personnaliser l'affichage de l'historique
+# Personnalisation de l'historique
 alias history="history | sed 's/\(.*[0-9]*\)/\x1b[32;1m\1\x1b[0m/; s/-- \([0-9/ :]*\) --/\x1b[38;5;214m-- \1 --\x1b[0m/'"
